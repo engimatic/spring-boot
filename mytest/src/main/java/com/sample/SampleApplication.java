8a5c79475389ba96864f33b1be8d98b3fe655d77
engimatic <eternity_bliss@sina.cn>
2021-01-26 22:02:47 +0800

    sample

---
+++ mytest/src/main/java/com/sample/SampleApplication.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: tianjin
+ * @email: eternity_bliss@sina.cn
+ * @create: 2021-01-26 22:01:45
+ */
+@SpringBootApplication
+public class SampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+}
